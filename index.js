23612b4f05a780f286da2f37581c82f6ae24d1f7
Martyna <martyna.glaz@gmail.com>
2019-04-10 01:38:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function happyHolidays(string){
   return ("Happy holidays!")
 }
-function happyHolidaysTo(Happy holidays,name){
+function happyHolidaysTo(name){
 
   return (`Happy holidays, ${name}!`)
 }
