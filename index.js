7cbb7f9d75a4367724e02ca2036a1ae70a871674
Martyna <martyna.glaz@gmail.com>
2019-04-10 01:53:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function happyHolidayTo(holiday,name){
   return (`Happy ${holiday}, ${name}!`)
 }
 function holidayCountdown(days,holiday){
-  return (`It's ${days} days until ${holiday}!`)||(`It's ${holiday} days until ${days}!`)
+  return (`It's ${days} days until ${holiday}!`)&&(`It's ${holiday} days until ${days}!`)
 
 }
